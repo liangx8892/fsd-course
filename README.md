5dd1f7004998417d878695c9eac5eac8136b6238
LIANG XIONG <xionglwh@cn.ibm.com>
2019-11-04 23:47:27 +0800

    init commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Course Services for FSD SBA.
---
+++ src/main/java/com/fsd/sba/CourseApplication.java
@@ -0,0 +1,13 @@
+package com.fsd.sba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class CourseApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(CourseApplication.class, args);
+	}
+}
